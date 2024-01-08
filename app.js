01a4c85814bce6922dd4db6a0c270520fc8633b1
user14871684 <153417797+user14871684@users.noreply.github.com>
2024-01-08 08:49:34 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const Containerr = document.getElementById("containerr")
+
+const Open = () => {
+    Containerr.style.transform="translate(0)"
+    Containerr.style.marginleft="400px"
+}
+const Close = () => {
+    Containerr.style.transform="translate(2000px)"
+}
